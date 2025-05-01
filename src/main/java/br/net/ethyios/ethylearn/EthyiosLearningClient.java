c0eb81fa24badf1b3ed9bd3a687d1dca93e5abd1
ethyios <gugagom1998@gmail.com>
2025-05-01 00:24:32 -0300

    Add EthyiosLearningClient implementation and update fabric.mod.json
    Signed-off-by: ethyios <gugagom1998@gmail.com>

---
+++ src/main/java/br/net/ethyios/ethylearn/EthyiosLearningClient.java
@@ -0,0 +1,10 @@
+package br.net.ethyios.ethylearn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EthyiosLearningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
