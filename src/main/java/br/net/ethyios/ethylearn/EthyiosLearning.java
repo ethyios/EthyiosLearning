593c9102fb595f0c57cfbe375891d11f3ceac762
ethyios <gugagom1998@gmail.com>
2025-05-01 00:30:51 -0300

    Register mod items during initialization
    Signed-off-by: ethyios <gugagom1998@gmail.com>

--- src/main/java/br/net/ethyios/ethylearn/EthyiosLearning.java
+++ src/main/java/br/net/ethyios/ethylearn/EthyiosLearning.java
@@ -1,5 +1,6 @@
 package br.net.ethyios.ethylearn;
 
+import br.net.ethyios.ethylearn.registration.EthyItemRegistration;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,5 +13,6 @@ public class EthyiosLearning implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing EthyiosLearning!");
+		EthyItemRegistration.registerModItems();
 	}
 }
